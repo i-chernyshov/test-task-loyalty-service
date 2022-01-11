1302752de135d9c61020641af702e3516c11e6b4
Vladimir <vch@burno.tech>
2022-01-11 18:36:29 +0300

    Update PHPDocs models properties and methods.

--- app/Models/LoyaltyPointsRule.php
+++ app/Models/LoyaltyPointsRule.php
@@ -2,8 +2,31 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\LoyaltyPointsRule
+ *
+ * @property int $id
+ * @property string $points_rule
+ * @property string $accrual_type
+ * @property float $accrual_value
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|LoyaltyPointsRule newModelQuery()
+ * @method static Builder|LoyaltyPointsRule newQuery()
+ * @method static Builder|LoyaltyPointsRule query()
+ * @method static Builder|LoyaltyPointsRule whereAccrualType($value)
+ * @method static Builder|LoyaltyPointsRule whereAccrualValue($value)
+ * @method static Builder|LoyaltyPointsRule whereCreatedAt($value)
+ * @method static Builder|LoyaltyPointsRule whereId($value)
+ * @method static Builder|LoyaltyPointsRule wherePointsRule($value)
+ * @method static Builder|LoyaltyPointsRule whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class LoyaltyPointsRule extends Model
 {
     public const ACCRUAL_TYPE_RELATIVE_RATE = 'relative_rate';
--- app/Models/LoyaltyPointsTransaction.php
+++ app/Models/LoyaltyPointsTransaction.php
@@ -2,8 +2,43 @@
 
 namespace App\Models;
 
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\LoyaltyPointsTransaction
+ *
+ * @property int $id
+ * @property int $account_id
+ * @property float $points_amount
+ * @property float|null $payment_amount
+ * @property string|null $payment_id
+ * @property int|null $payment_time
+ * @property string $description
+ * @property int|null $points_rule
+ * @property int $canceled
+ * @property string|null $cancellation_reason
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|LoyaltyPointsTransaction newModelQuery()
+ * @method static Builder|LoyaltyPointsTransaction newQuery()
+ * @method static Builder|LoyaltyPointsTransaction query()
+ * @method static Builder|LoyaltyPointsTransaction whereAccountId($value)
+ * @method static Builder|LoyaltyPointsTransaction whereCanceled($value)
+ * @method static Builder|LoyaltyPointsTransaction whereCancellationReason($value)
+ * @method static Builder|LoyaltyPointsTransaction whereCreatedAt($value)
+ * @method static Builder|LoyaltyPointsTransaction whereDescription($value)
+ * @method static Builder|LoyaltyPointsTransaction whereId($value)
+ * @method static Builder|LoyaltyPointsTransaction wherePaymentAmount($value)
+ * @method static Builder|LoyaltyPointsTransaction wherePaymentId($value)
+ * @method static Builder|LoyaltyPointsTransaction wherePaymentTime($value)
+ * @method static Builder|LoyaltyPointsTransaction wherePointsAmount($value)
+ * @method static Builder|LoyaltyPointsTransaction wherePointsRule($value)
+ * @method static Builder|LoyaltyPointsTransaction whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class LoyaltyPointsTransaction extends Model
 {
     protected $table = 'loyalty_points_transaction';
@@ -40,7 +75,8 @@ class LoyaltyPointsTransaction extends Model
         ]);
     }
 
-    public static function withdrawLoyaltyPoints($account_id, $points_amount, $description) {
+    public static function withdrawLoyaltyPoints($account_id, $points_amount, $description)
+    {
         return LoyaltyPointsTransaction::create([
             'account_id' => $account_id,
             'points_rule' => 'withdraw',
